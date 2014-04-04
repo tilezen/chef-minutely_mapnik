361edb02fb1d8b9f275fae88f61dea0362461ef7
Grant Heffernan <heffergm@gmail.com>
2014-04-04 12:11:06 -0400

    Revert "fix cron bug"
    This reverts commit 1ea3308c9e503d70c25c69f2c2f2f47cad71b34b.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'All rights reserved'
 description      'Installs/Configures mapzen_minutely_mapnik'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '0.0.1'
 
 %w(
   osmosis
